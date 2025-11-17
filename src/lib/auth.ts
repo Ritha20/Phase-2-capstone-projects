6b1d4dc441c57acc7c15f3bbf98ced6e84e17f6a
Irakoze Ritha Theonestine <irakozeritha20@gmail.comclear>
2025-11-17 11:40:30 +0200

    created the hashed password

---
+++ src/lib/auth.ts
@@ -0,0 +1,11 @@
+// src/lib/auth.ts
+import bcrypt from 'bcryptjs';
+
+export async function hashPassword(password: string): Promise<string> {
+  const saltRounds = 12;
+  return await bcrypt.hash(password, saltRounds);
+}
+
+export async function verifyPassword(password: string, hashedPassword: string): Promise<boolean> {
+  return await bcrypt.compare(password, hashedPassword);
+}
