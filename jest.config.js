ea935a6036c5acddb68302fc5de07a8388ba1f2c
Irakoze Ritha Theonestine <irakozeritha20@gmail.comclear>
2025-11-21 10:53:05 +0200

    fixed: vercel function patterns

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,3 @@
-// jest.config.js
 const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
@@ -8,7 +7,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jsdom',
-  moduleNameMapping: {  // FIXED: Changed from moduleNameMapping to moduleNameMapping
+  moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
